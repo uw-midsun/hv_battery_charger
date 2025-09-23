3ed48b2e2daa637ae1d38bff6004724d2f31d39a
foehammer127 <foehammer@disroot.org>
2025-09-23 16:50:03 -0400

    Implement missing syscalls.
    Add definitions for missing syscalls.

---
+++ board/src/syscalls.c
@@ -0,0 +1,13 @@
+/************************************************************************************************
+ * @file   syscalls.c
+ *
+ * @brief Implement missing syscall stubs.
+ *
+ * @date   2025-09-23
+ * @author Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+void _close_r(void) {}
+void _lseek_r(void) {}
+void _read_r(void) {}
+void _write_r(void) {}
