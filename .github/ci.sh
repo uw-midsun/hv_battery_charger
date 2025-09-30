592c8c15e0651777ca77abf1e185d23bcdca27d9
foehammer127 <foehammer@disroot.org>
2025-09-30 18:21:39 -0400

    Add pipefail to ci.

--- .github/ci.sh
+++ .github/ci.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 cmake -B build -G Ninja
 
 pushd build
--- .github/setup.sh
+++ .github/setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash 
 
+set -euo pipefail
+
 sudo apt-get install -y binutils-arm-none-eabi gcc-arm-none-eabi
 sudo apt-get install -y gcc-11 g++-11 clang clang-format gdb
 sudo apt-get install -y cmake make ninja-build
