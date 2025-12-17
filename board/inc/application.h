e9cba9b2ecb2e1921a0755be1c3f7822ce8c3377
Lorenzo Good <foehammer@disroot.org>
2025-12-17 17:51:18 -0600

    Add firmware.

--- board/inc/application.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-typedef struct {
-} Application;
-
-void application_init(Application *application);
-
-void application_run(Application *application);
--- board/src/main.c
+++ board/src/main.c
@@ -1,13 +1,4 @@
-#include "application.h"
-
-static Application application;
-
-void main() {
-  application_init(&application);
-  while (1) {
-    application_run(&application);
-  }
-}
+void main() {}
 
 #ifdef USE_FULL_ASSERT
 void assert_failed(uint8_t* file, uint32_t line) {}
