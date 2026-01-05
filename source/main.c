2497b4af6a8a06dd743cd3b4098c1ff992eb0f14
Lorenzo Good <foehammer@disroot.org>
2026-01-04 23:30:25 -0600

    Set up source dir.

---
+++ source/main.c
@@ -0,0 +1,8 @@
+void main() {
+  while (1) {
+  }
+}
+
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t* file, uint32_t line) {}
+#endif
---
+++ source/syscalls.c
@@ -0,0 +1,5 @@
+// Implement required syscalls to silence build errors.
+void _close_r(void) {}
+void _lseek_r(void) {}
+void _read_r(void) {}
+void _write_r(void) {}
