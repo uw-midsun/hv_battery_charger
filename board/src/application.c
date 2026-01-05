f331e004bcc7862abc7a0739b4ed85995f762a59
Lorenzo Good <foehammer@disroot.org>
2026-01-04 23:20:23 -0600

    Start over.

--- board/src/application.c
+++
@@ -1,7 +0,0 @@
-#include "application.h"
-
-#include "stm32f3xx_hal.h"
-
-void application_init(Application *application) { HAL_Init(); }
-
-void application_run(Application *application) { HAL_Delay(1000); }
